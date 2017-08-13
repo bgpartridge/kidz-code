d14dfcfaca7ce6bd6431c8fd6a7d38e1cbba3b22
Brett Partridge <brett.partridge@infusionsoft.com>
2017-08-12 23:38:19 -0700

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 scp -i ~/.ssh/id_rsa_partridgefam index.html u45524108@partridgefam.com:~/kidz-code/
+scp -i ~/.ssh/id_rsa_partridgefam -r ./hyrum u45524108@partridgefam.com:~/kidz-code/hyrum/
