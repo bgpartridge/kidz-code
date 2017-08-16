c29eddefacc8e3d26305bec2b63a1e24edad562c
Brett Partridge <brett.partridge@infusionsoft.com>
2017-08-15 23:32:23 -0700

    hooking up all buttons; connecting to save endpoint; styled code mirror

--- calculator.js
+++ calculator.js
@@ -1,3 +1,59 @@
-function numberOne(value) {
+window.Zero = function(value) {
+	alert(value)
+}
+
+window.One = function(value) {
+	alert(value)
+}
+
+window.Two = function(value) {
+	alert(value)
+}
+
+window.Three = function(value) {
+	alert(value)
+}
+
+window.Four = function(value) {
+	alert(value)
+}
+
+window.Five = function(value) {
+	alert(value)
+}
+
+window.Six = function(value) {
+	alert(value)
+}
+
+window.Seven = function(value) {
+	alert(value)
+}
+
+window.Eight = function(value) {
+	alert(value)
+}
+
+window.Nine = function(value) {
+	alert(value)
+}
+
+window.Add = function(value) {
+	alert(value)
+}
+
+window.Subtract = function(value) {
+	alert(value)
+}
+
+window.Multiply = function(value) {
+	alert(value)
+}
+
+window.Divide = function(value) {
+	alert(value)
+}
+
+window.Equals = function(value) {
 	alert(value)
 }
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+scp -i ~/.ssh/id_rsa_partridgefam save.php u45524108@partridgefam.com:~/kidz-code/
 scp -i ~/.ssh/id_rsa_partridgefam index.html u45524108@partridgefam.com:~/kidz-code/
-scp -i ~/.ssh/id_rsa_partridgefam calculator.js u45524108@partridgefam.com:~/kidz-code/
+#scp -i ~/.ssh/id_rsa_partridgefam calculator.js u45524108@partridgefam.com:~/kidz-code/
 scp -i ~/.ssh/id_rsa_partridgefam -r ./hyrum u45524108@partridgefam.com:~/kidz-code/hyrum/
